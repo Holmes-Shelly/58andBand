161fc3e6deb0eb727af4ba33c3a3c734c793eef8
Shi Hao <shihao06@gmail.com>
2019-01-18 10:06:33 +0800

    Add files via upload

--- 0116.py
+++ 0116.py
@@ -10,7 +10,7 @@ from email.header import Header
 from fontTools.ttLib import TTFont
 
 req = requests.session()
-url = 'https://rongcheng.58.com/whrcsq/zufang/j3/?PGTID=0d3090a7-046e-413b-b067-6af5194bd063&ClickID=1'
+url = 'https://rongcheng.58.com/whrcsq/zufang/0/j3/?PGTID=0d300008-046e-47a7-d532-f58bb1b28b2a&ClickID=2'
 
 headers = {
 	'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -18,7 +18,7 @@ headers = {
 	'accept-language':'en,zh;q=0.9,zh-CN;q=0.8,lb;q=0.7',
 	'Cache-Control':'max-age=0',
 	'Upgrade-Insecure-Requests':'1',
-	'cookie':'commontopbar_new_city_info=522%7C%E8%8D%A3%E6%88%90%7Crongcheng; f=n; commontopbar_ipcity=qd%7C%E9%9D%92%E5%B2%9B%7C0; id58=c5/njVphg4qNMyALEOAkAg==; 58tj_uuid=e07705a3-a222-4f05-aac6-ab7f5bb6f7f5; als=0; __utma=253535702.793148155.1516340160.1516340160.1516340160.1; xxzl_deviceid=gG55ora%2BTdnWecKNRDJzkKd2icRHcLtd44Z74COlIHDv6FjW4PKJRS5m6sHIFRZ8; wmda_new_uuid=1; wmda_uuid=269d53e36c005c6ac5e3cef82bc651c2; wmda_visited_projects=%3B2385390625025; xxzl_smartid=6f40ca08ae1efe071b5b803a8fa806f7; city=qd; 58home=qd; ppStore_fingerprint=9D48D6955935981E84F4C6EA04661652A0639E72051CB01F%EF%BC%BF1547554785806; new_uv=16; utm_source=; spm=; init_refer=; wmda_session_id_2385390625025=1547637784967-a68bae6f-b759-d099; f=n; new_session=0; xzfzqtoken=VmQYUAsdWXDMPAHE1klkpAnZ0rUuSzLosQww3mIX8lpmfL%2BsYevhcBxwwesiqNWKin35brBb%2F%2FeSODvMgkQULA%3D%3D',
+	'cookie':'f=n; commontopbar_new_city_info=522%7C%E8%8D%A3%E6%88%90%7Crongcheng; commontopbar_ipcity=qd%7C%E9%9D%92%E5%B2%9B%7C0; id58=c5/njVphg4qNMyALEOAkAg==; 58tj_uuid=e07705a3-a222-4f05-aac6-ab7f5bb6f7f5; als=0; __utma=253535702.793148155.1516340160.1516340160.1516340160.1; xxzl_deviceid=gG55ora%2BTdnWecKNRDJzkKd2icRHcLtd44Z74COlIHDv6FjW4PKJRS5m6sHIFRZ8; wmda_new_uuid=1; wmda_uuid=269d53e36c005c6ac5e3cef82bc651c2; wmda_visited_projects=%3B2385390625025; xxzl_smartid=6f40ca08ae1efe071b5b803a8fa806f7; city=qd; 58home=qd; new_uv=17; utm_source=; spm=; init_refer=; wmda_session_id_2385390625025=1547776923833-bb05eee0-b2ca-b761; new_session=0; f=n; defraudName=defraud; ppStore_fingerprint=9D48D6955935981E84F4C6EA04661652A0639E72051CB01F%EF%BC%BF1547777040702; xzfzqtoken=EmwgnF4awumh5uWVjaePK%2FKF852S0Ix8v5LytN1DdeaQ7qDK6PAZwNCW8TPPoW8vin35brBb%2F%2FeSODvMgkQULA%3D%3D',
 	'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 	}
 
